318e9b43f62317335075e98b747c76ae93254b4d
David Silverthorn <david.martin.silverthorn@gmail.com>
2021-07-01 19:20:24 -0500

    Changes to ExpressJS and package.json
    * Added cookie-parser to Express
    
    * Added urlencoded and json parsers to Express
    
    * Added cross-env to monorepo
    
    * tweaked scripts in package.json

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const server = express()
-const { resolve } = require('path')
+const { resolve } = require('path');
+const cookieParser = require('cookie-parser')
+const express = require('express');
+const server = express();
 require('dotenv').config()
 
+const distDir = resolve(process.cwd(), '../client/dist');
 const port = (process.env.SERVER_PORT) ? process.env.SERVER_PORT : 3000;
-const distDir = resolve(process.cwd(), '../client/dist')
+const production = (process.env.NODE_ENV === 'production') ? true : false;
 
+server.use(cookieParser())
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 server.use(express.static(distDir))
 
 server.get('/*', (req, res) => {
